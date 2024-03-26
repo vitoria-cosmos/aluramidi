c51e022919db87321d4286385bbcaf32a5ba59a4
unknown <maria.v.cosmo@gmail.com>
2024-03-26 18:32:18 -0300

    atualizando branch

--- main.js
+++ main.js
@@ -1,5 +1,10 @@
-document.querySelector('.tecla_pom');
 
-function apertou() {
-    alert('Apertou!');
+function tocaSomPom() {
+    document.querySelector('#som_tecla_pom').play();  
+}
+
+document.querySelector('.tecla_pom').onclick = tocaSomPom;
+
+function tocaSomClap() {
+    document.querySelector('#som_tecla_clap').play();
 }
