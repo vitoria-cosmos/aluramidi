ddb18769ea8107060ff788b14707768e52a93099
unknown <maria.v.cosmo@gmail.com>
2024-03-27 10:09:46 -0300

    Sons dos teclados funcionando!

--- main.js
+++ main.js
@@ -1,10 +1,33 @@
+function tocaSom(idElementoAudio) {
 
-function tocaSomPom() {
-    document.querySelector('#som_tecla_pom').play();  
+    document.querySelector(idElementoAudio).play();  
 }
 
-document.querySelector('.tecla_pom').onclick = tocaSomPom;
+// document.querySelector('.tecla_pom').onclick = tocaSomPom;
 
-function tocaSomClap() {
-    document.querySelector('#som_tecla_clap').play();
+ const listaDeTeclas = document.querySelectorAll('.tecla');
+
+// listaDeTeclas[0].onclick = tocaSomPom;
+
+// function tocaSomClap() {
+//     document.querySelector('#som_tecla_clap').play();
+// }
+
+
+let contador = 0;
+// enquanto
+while (contador < listaDeTeclas.length) {
+
+    const tecla = listaDeTeclas[contador];
+
+    const instrumento = tecla.classList[1];
+
+    // template string
+    const idAudio = `#som_${instrumento}`;
+
+    tecla.onclick = function() {
+        tocaSom(idAudio);
+    }
+    contador = contador + 1;
+    console.log(contador);
 }
