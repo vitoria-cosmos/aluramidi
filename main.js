51b60c827b4bcabde6ac6ce1715c28803d519381
vitoria.sousa <maria.v.cosmo@gmail.com>
2024-05-17 12:40:26 -0300

    aula02

--- main.js
+++ main.js
@@ -1,3 +1,18 @@
 // alert('Olá, mundo');
 
-document.querySelector('.tecla_pom');
+// document.querySelector('.tecla_pom');
+
+// document.querySelector('#som_tecla_pom').play();
+
+function tocaSomPom() {
+    document.querySelector('#som_tecla_pom').play();
+}
+
+function tocaSomClap() {
+    document.querySelector('#som_tecla_clap').play();
+}
+
+// document.querySelector('.tecla_pom').onclick = tocaSomPom();
+document.querySelector('.tecla_pom').onclick = tocaSomPom;
+
+
