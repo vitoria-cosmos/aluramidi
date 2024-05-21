2714f3f975765f9d98fec3638fc64086b36a5965
vitoria.sousa <maria.v.cosmo@gmail.com>
2024-05-20 23:04:33 -0300

    função anônima

--- main.js
+++ main.js
@@ -1,16 +1,18 @@
 
-function tocaSomPom() {
-    document.querySelector('#som_tecla_pom').play();
+function tocaSom(idElementoAudio) {
+    document.querySelector(idElementoAudio).play();
 }
-
-function tocaSomClap() {
-    document.querySelector('#som_tecla_clap').play();
-}
-
-
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
-listaDeTeclas[0].onclick = tocaSomPom;
+let contador = 0;
+// enquanto
+while (contador < listaDeTeclas.length) {
+    listaDeTeclas[contador].onclick = function() {
+        tocaSom('#som_tecla_pom');
+    }
 
-listaDeTeclas[1].onclick = tocaSomClap;
+    contador = contador + 1;
+
+    console.log(contador);
+} 
