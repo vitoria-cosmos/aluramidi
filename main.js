3352feccf67cbf37ad4e4d8a6b5477dfbb6c06e5
vitoria.sousa <maria.v.cosmo@gmail.com>
2024-05-22 15:58:09 -0300

    trabalhando com possiveis erros

--- main.js
+++ main.js
@@ -1,6 +1,17 @@
 
-function tocaSom(idElementoAudio) {
-    document.querySelector(idElementoAudio).play();
+function tocaSom(seletorAudio) {
+    const elemento = document.querySelector(seletorAudio);
+
+    if (elemento == null) {
+        // alert('Elemento não encontrado');
+        console.log('Elemento não encontrado');
+
+    }
+    if (elemento != null) {
+        if (elemento.localName === 'audio') {
+            elemento.play();
+        }
+    }
 }
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
@@ -39,8 +50,8 @@ for (let contador = 0; contador < listaDeTeclas.length; contador ++) {
     }
 
     tecla.onkeydown = function(evento) {
-        console.log(evento.code === 'Space');
-        if (evento.code == 'Space' ) {
+        // console.log(evento.code === 'Space');
+        if (evento.code == 'Space' || evento.code == 'Enter') {
             tecla.classList.add('ativa'); 
         }
         
